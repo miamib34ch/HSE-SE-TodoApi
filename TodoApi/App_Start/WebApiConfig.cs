c73db4aa89e10ce4bd527b278ae83e410b8f58ec
Богдан Полыгалов <bapolygalov@edu.hse.ru>
2022-06-06 21:04:11 +0500

    добавил удаление и отображение количества дел

--- TodoApi/App_Start/WebApiConfig.cs
+++ TodoApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers;
+
 
 namespace TodoApi
 {
@@ -19,6 +21,9 @@ namespace TodoApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
--- TodoApi/Controllers/HomeController.cs
+++ TodoApi/Controllers/HomeController.cs
@@ -10,8 +10,6 @@ namespace TodoApi.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
             return View();
         }
     }
